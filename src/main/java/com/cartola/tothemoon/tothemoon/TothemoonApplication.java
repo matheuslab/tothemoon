dc3b54f692d7ee0be0ba75e9e7f2010fb9f9b94b
Matheus Martins <matheus.martins@ee.ufcg.edu.br>
2020-02-09 14:05:53 -0300

    Creating Spring Project

---
+++ src/main/java/com/cartola/tothemoon/tothemoon/TothemoonApplication.java
@@ -0,0 +1,13 @@
+package com.cartola.tothemoon.tothemoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TothemoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TothemoonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cartola/tothemoon/tothemoon/TothemoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cartola.tothemoon.tothemoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TothemoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
